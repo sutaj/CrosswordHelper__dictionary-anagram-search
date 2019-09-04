81630c6304661c21f8591714ae4c02bd26e6ff08
Akil <krs.akil@gmail.com>
2019-09-04 14:13:12 +0000

    code update
    Search pattern, strings in resource to easy translation, input checks

--- Program.cs
+++ Program.cs
@@ -17,84 +17,138 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Slowa
 {
     class Program
     {
-        static string litery; // jakie litery
-        static int ileOD, ileDO; // ile liter
-        static string slownik = "slowa.txt"; // plik slownika
-        static List<string> slowa; // lista ze slowami
-        static bool dupl = false; // czy moga wystepowac duplikaty
+        static string letters; // jakie litery
+        static int lettersMin, lettersMax; // ile liter
+        static string dictionaryFile = "slowa.txt"; // plik slownika
+        static List<string> wordDic; // lista ze slowami
+        static bool useDuplicates = false; // czy moga wystepowac duplikaty
+        static int min_letters = 3;
+        static bool usepatt = false;
+        static string pat;
+
+        #region Yes/No keys
+        static ConsoleKey kYES = ConsoleKey.T;
+        //static ConsoleKey kNO = ConsoleKey.N;
+        #endregion
 
         static void Main(string[] args)
         {
-            Start();
+            if (File.Exists(dictionaryFile))
+            {
+                wordDic = new List<string>();
+                Start();
+            }
+            else
+            {
+                Console.WriteLine(string.Format(Properties.Resource.sERR_NO_DICTIONARY_FILE_FOUND, dictionaryFile));
+                Console.ReadKey();
+            }
         }
 
         static void Start()
         {
             ConsoleKeyInfo duplikaty = new ConsoleKeyInfo();
+            ConsoleKeyInfo searchpatt = new ConsoleKeyInfo();
 
+            Console.Title = "Pomocnik krzyżówkowicza.";
             Console.Clear();
-            Console.WriteLine($"\t\t Pomocnik krzyżówkowicza. ver. {Assembly.GetExecutingAssembly().GetName().Version}{Environment.NewLine}\t\t\t\tSłownik sjp.pl{Environment.NewLine}\t\t\thttps://github.com/sutaj{Environment.NewLine}");
-            Console.Write("Z jakich liter?: \t\t");
-            litery = Console.ReadLine();
+            #region baner
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"\t\t Pomocnik krzyżówkowicza. ver. {Assembly.GetExecutingAssembly().GetName().Version}{Environment.NewLine}\t\t\t\t" +
+                $"Słownik sjp.pl{Environment.NewLine}\t\t\t");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"https://github.com/sutaj {Environment.NewLine}");
+            Console.ResetColor();
+            #endregion
 
-            if (litery.Length < 2)
+            Console.Write($" > {Properties.Resource.sWHAT_LETTERS}: \t\t\t");
+
+            letters = Console.ReadLine();
+
+            if (letters.Length < min_letters)
             {
-                Blad($"Za mało znaków ({litery.Length}). Minimum to 3.");
+                Error(String.Format(Properties.Resource.sERROR_MORE_LETTERS, letters.Length , min_letters));
             }
 
-            Console.Write("Ilość znaków między [od]: \t");
-
-            if (!int.TryParse(Console.ReadLine(), out ileOD))
+            if(Regex.IsMatch(letters, @"\d"))
             {
-                Blad("To musi być liczba.");
+                Error(string.Format(Properties.Resource.sERR_NO_NUMBERS_ALLOWED, letters.ToUpper()));
+            }
+
+            Console.Write($" > {Properties.Resource.sUSE_SEARCH_PATTERN} ");
+            searchpatt = Console.ReadKey(true);
+            if (searchpatt.Key == kYES)
+            {
+                Console.WriteLine($"\t{Properties.Resource.sYES}");
+                Console.WriteLine($"\t{Properties.Resource.sSEARCH_TIP}");
+                Console.Write($" > {Properties.Resource.sSEARCH_PATTERN}:\t\t");
+                Console.Write(" > ");
+                pat = Console.ReadLine();
+                usepatt = true;
             }
             else
             {
-                if (ileOD > litery.Length)
-                {
-                    Blad("Nie może być większa niż liczba liter.");
-                }
+                Console.WriteLine($"\t{Properties.Resource.sNO}");
+                usepatt = false;
             }
 
-            Console.Write("a [do]: \t\t\t");
-            if (!int.TryParse(Console.ReadLine(), out ileDO))
+            Console.Write($" > {Properties.Resource.sMIN_LENGHT}: \t\t");
+
+            if (!int.TryParse(Console.ReadLine(), out lettersMin))
             {
-                Blad("To musi być liczba.");
+                Error($"{Properties.Resource.sERR_NEED_NUMBER}");
             }
             else
             {
-                if (ileDO < ileOD)
+                if (lettersMin > letters.Length)
                 {
-                    Blad("Maksymalna ilość znaków nie może być mniejsza niż minimalna.");
+                    Error($"{Properties.Resource.sERR_TO_BIG_NUMBER}.");
                 }
             }
 
-            Console.Write("Pozwolić na duplikaty znaków [T/N]? ");
+            Console.Write($" > {Properties.Resource.sMAX_LENGHT}: \t\t\t\t");
+            if (!int.TryParse(Console.ReadLine(), out lettersMax))
+            {
+                Error(Properties.Resource.sERR_NEED_NUMBER);
+            }
+            else
+            {
+                if (lettersMax < lettersMin)
+                {
+                    Error(Properties.Resource.sERR_MAX_NUMBER_TO_SMALL);
+                }
+            }
+
+            Console.Write($" > {Properties.Resource.sALLOW_DUPLICATES} ");
             duplikaty = Console.ReadKey(true);
 
-            if (duplikaty.Key == ConsoleKey.T)
+            if (duplikaty.Key == kYES)
             {
-                Console.WriteLine("\t TAK");
-                dupl = true;
+                Console.WriteLine($"\t{Properties.Resource.sYES.ToUpper()}");
+                useDuplicates = true;
             }
             else
             {
-                Console.WriteLine("\t NIE");
-                dupl = false;
+                Console.WriteLine($"\t{Properties.Resource.sNO.ToUpper()}"); 
+                useDuplicates = false;
             }
 
-            Console.WriteLine($"{Environment.NewLine}---------------------------------------------------------");
-            RobSwoje();
+            Console.WriteLine($"{Environment.NewLine} ──────────────────────────────────────────────────────────{Environment.NewLine}");
+            doSomeWork();
         }
         
-        static void Blad(string msg)
+        static void Error(string msg)
         {
-            Console.WriteLine($"\t {msg}.{Environment.NewLine}\t Dowolny klawisz by zacząć od nowa.");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"\t {msg}.{Environment.NewLine}");
+            Console.ResetColor();
+            Console.WriteLine($"\t {Properties.Resource.sPRESSKEY}");
             Console.ReadKey(true);
             Start();
         }
@@ -102,106 +156,151 @@ namespace Slowa
         static string liter(int ile)
         {
             string _ret;
+
             if(ile == 1)
             {
-                _ret = "litera";
+                _ret = Properties.Resource.sLETTER1;
             }
             else if(ile > 1 && ile <5)
             {
-                _ret = "litery";
+                _ret = Properties.Resource.sLETTER2;
             }
             else
             {
-                _ret = "liter";
+                _ret = Properties.Resource.sLETTER3;
             }
 
             return _ret;
         }
 
-        static void RobSwoje()
+        static void doSomeWork()
         {
-            ConsoleKeyInfo jeszczeraz = new ConsoleKeyInfo();
+            ConsoleKeyInfo doAgain = new ConsoleKeyInfo();
             
-            slowa = new List<string>();
-            List<string> match = new List<string>();
-            Console.Write("  > Tworzę słownik... ");
+            Console.Write($"  > {Properties.Resource.sCREATING_DICTIONARY}... ");
             int counter = 0;
 
-            foreach (string item in File.ReadAllLines(slownik))
+            if (wordDic.Count < 1)
             {
-                slowa.Add(item);
+                try
+                {
+                    foreach (string item in File.ReadAllLines(dictionaryFile))
+                    {
+                        wordDic.Add(item);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Error(string.Format(Properties.Resource.sERR_DIC_LOAD, ex.Message));
+                }
             }
 
-            Console.Write($"{slowa.Count} słów w słowniku.");
+            Console.Write($"{wordDic.Count} {Properties.Resource.sWORDS_IN_DICTIONARY}.");
 
-            for (int i = ileOD; i <= ileDO; i++)
+            for (int i = lettersMin; i <= lettersMax; i++)
             {
                 int row = 0;
                 int cnt = 0;
-
                 Console.Write($"{Environment.NewLine}{Environment.NewLine} [{i} {liter(i)}]:{Environment.NewLine}");
 
-                foreach (string item in slowa)
+                if (!usepatt)
                 {
-                    if (SprawdzTo(litery.ToLower(), item.ToLower(), dupl))
+                    #region normal search
+                    foreach (string item in wordDic)
                     {
-                        if (item.Length == i)
+                        //m = Regex.Match(item, pattern, RegexOptions.IgnoreCase);
+                        if (CheckIt(letters.ToLower(), item.ToLower(), useDuplicates))
                         {
-                            counter++;
-                            if (row < 6)
+                            if (item.Length == i)
                             {
-                                Console.Write($" {item}\t");
-                                row++;
-                                cnt++;
-                            }
-                            else
-                            {
-                                Console.Write($" {item}{Environment.NewLine}");
-                                row = 0;
-                                cnt++;
+                                counter++;
+                                if (row < 6)
+                                {
+                                    Console.Write($" {item}\t");
+                                    row++;
+                                    cnt++;
+                                }
+                                else
+                                {
+
+                                    Console.Write($" {item}{Environment.NewLine}");
+                                    row = 0;
+                                    cnt++;
+                                }
                             }
                         }
                     }
+                    #endregion
+                }
+                else
+                {
+                    #region search with pattern
+                    string pattern = string.Empty;
+                    Match m;
+                    pattern = $"\\b{pat.Replace("*", "\\w")}\\b"; //"\ba\w*\b";    
+
+                    foreach (string item in wordDic)
+                    {
+                        m = Regex.Match(item, pattern, RegexOptions.IgnoreCase);
+                        if (CheckIt(letters.ToLower(), item.ToLower(), useDuplicates) && m.Success)
+                        {
+                            if (item.Length == i)
+                            {
+                                counter++;
+                                if (row < 6)
+                                {
+                                    Console.Write($"{item}\t");
+                                    row++;
+                                    cnt++;
+                                }
+                                else
+                                {
+
+                                    Console.Write($"{item}{Environment.NewLine}");
+                                    row = 0;
+                                    cnt++;
+                                }
+                            }
+                        }
+                    }
+                    #endregion
                 }
 
+                #region counter is 0
                 if (cnt < 1)
                 {
-                    Console.Write("\t=brak=");
+                    Console.Write($"\t={Properties.Resource.sEMPTY}=");
                 }
+                #endregion
             }
 
-            Console.WriteLine($"{Environment.NewLine}  > Znaleziono {counter} słów.");
+            Console.WriteLine(string.Format(Environment.NewLine + Properties.Resource.sWORDS_FOUND, counter, Environment.NewLine));
 
-            Console.WriteLine($"{Environment.NewLine}{Environment.NewLine} \t Jeszcze raz [T/N]? ");
-            jeszczeraz = Console.ReadKey();
-            if (jeszczeraz.Key == ConsoleKey.T)
+            Console.WriteLine($"{Environment.NewLine}{Environment.NewLine} \t {Properties.Resource.sAGAIN}? ");            
+
+            doAgain = Console.ReadKey();
+            if (doAgain.Key == kYES)
             {
                 Start();
             }
             
         }
 
-
-        static bool SprawdzTo(string slowo1, string slowo2)
+        #region crude serch
+        static bool CheckIt(string string1, string string2, bool allowDuplicateLetters)
         {
-            var chars = new HashSet<char>(slowo1);
-            return slowo2.All(x => chars.Contains(x));
-        }
-
-        static bool SprawdzTo(string slowo1, string slowo2, bool DuplikatyLiter)
-        {
-            if (DuplikatyLiter == true)
+            if (allowDuplicateLetters == true)
             {
-                var chars = new HashSet<char>(slowo1);
-                return slowo2.All(x => chars.Contains(x));
+                var chars = new HashSet<char>(string1);
+                return string2.All(x => chars.Contains(x));
             }
             else
             {
-                var chars = slowo1.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
-                return slowo2.GroupBy(x => x).All(g => chars.ContainsKey(g.Key) && chars[g.Key] >= g.Count());
+                var chars = string1.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+                return string2.GroupBy(x => x).All(g => chars.ContainsKey(g.Key) && chars[g.Key] >= g.Count());
             }
         }
-
+        #endregion
 
     }
 }
